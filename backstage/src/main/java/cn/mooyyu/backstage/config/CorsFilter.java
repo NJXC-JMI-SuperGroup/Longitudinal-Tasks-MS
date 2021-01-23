d627d321f26ecc26792fc0ce051c0840b1b51750
mooyyu <mooyyucrab@icloud.com>
2021-01-23 21:21:59 +0800

    fix cors

--- backstage/src/main/java/cn/mooyyu/backstage/config/CorsFilter.java
+++ backstage/src/main/java/cn/mooyyu/backstage/config/CorsFilter.java
@@ -21,10 +21,10 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
         response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
-        response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
